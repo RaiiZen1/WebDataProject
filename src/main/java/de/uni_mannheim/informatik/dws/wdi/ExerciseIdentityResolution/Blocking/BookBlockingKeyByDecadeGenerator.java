d4149d76212b229e95b892bc4319422261f407cf
Markus Johannes Herre <mherre@mail.uni-mannheim.de>
2023-11-16 21:18:34 +0100

    Test

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/BookBlockingKeyByDecadeGenerator.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/BookBlockingKeyByDecadeGenerator.java
@@ -9,23 +9,19 @@ import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
 
-
-public class BookBlockingKeyByTitleGenerator2 extends
+public class BookBlockingKeyByDecadeGenerator extends
 		RecordBlockingKeyGenerator<Book, Attribute> {
 
 	private static final long serialVersionUID = 1L;
 
 
 	/* (non-Javadoc)
-	 * Generate blocking keys based on the first two letters of the title
+	 * @see de.uni_mannheim.informatik.wdi.matching.blocking.generators.BlockingKeyGenerator#generateBlockingKeys(de.uni_mannheim.informatik.wdi.model.Matchable, de.uni_mannheim.informatik.wdi.model.Result, de.uni_mannheim.informatik.wdi.processing.DatasetIterator)
 	 */
 	@Override
 	public void generateBlockingKeys(Book record, Processable<Correspondence<Attribute, Matchable>> correspondences,
 			DataIterator<Pair<String, Book>> resultCollector) {
-			
-		String blockingKeyValue = record.getTitle().substring(0, Math.min(2,record.getTitle().length())).toUpperCase();
-		resultCollector.next(new Pair<>(blockingKeyValue, record));
-		
+		resultCollector.next(new Pair<>(Integer.toString(record.getPublicationDate().getYear() / 10), record));
 	}
 
 }
--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/model/BookXMLReader.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/model/BookXMLReader.java
@@ -1,16 +1,14 @@
 package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
-import java.util.Locale;
 
+import org.apache.jena.sparql.function.library.date;
 import org.w3c.dom.Node;
 
 import de.uni_mannheim.informatik.dws.winter.model.DataSet;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
+import de.uni_mannheim.informatik.dws.winter.preprocessing.datatypes.DateJavaTime;
 
 public class BookXMLReader extends XMLMatchableReader<Book, Attribute>{
 
@@ -30,50 +28,13 @@ public class BookXMLReader extends XMLMatchableReader<Book, Attribute>{
         book.setGenres(getListFromChildElement(node, "genres"));
         book.setPublisher(getValueFromChildElement(node, "publisher"));
 
-        // PublicationDate publicationDate = new PublicationDateXMLReader().createModelFromElement(node, "publication_date");
-        // System.out.println(publicationDate.getPublicationDate());
-        //System.out.println(getValueFromChildElement(node, "publication_date");
-
+        String[] dateString = getValueFromChildElement(node, "publication_date").split("\n");
         try {
-            String dateString = getValueFromChildElement(node, "publication_date");
-    
-            if (dateString != null && !dateString.isEmpty()) {
-                String[] lines = getValueFromChildElement(node, "publication_date").split("\n");
-                String year = lines[0].trim();
-                // Default to January 1st if only the year is provided
-                String date = year + "-01-01";
-                // If the year does not have 4 digits add 0s to the front
-                if (year.length() == 2) {
-                    date = "00" + date;
-                } else if (year.length() == 3) {
-                    date = "0" + date;
-                    System.out.println(getValueFromChildElement(node, "title"));
-                } else if (year.length() == 1) {
-                    date = "000" + date;
-                }
-                DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-                        .appendPattern("yyyy-MM-dd")
-                        .parseDefaulting(ChronoField.CLOCK_HOUR_OF_DAY, 0)
-                        .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-                        .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-                        .toFormatter(Locale.ENGLISH);
-        
-                LocalDateTime dt = LocalDateTime.parse(date, formatter);
-                book.setPublicationDate(dt);
-            }
+            LocalDateTime dateTime = DateJavaTime.parse(dateString[0]);
+            book.setPublicationDate(dateTime);     
         } catch (Exception e) {
-            e.printStackTrace();
+            book.setPublicationDate(LocalDateTime.of(1970, 1, 1, 0, 0));
         }
-        
-
-
-
-
-
-
-
-
-
 
         String averageRating = getValueFromChildElement(node, "averageRating");
         if(averageRating != null && !averageRating.isEmpty()) {
@@ -98,5 +59,4 @@ public class BookXMLReader extends XMLMatchableReader<Book, Attribute>{
 
         return book;
     }
-    
 }
